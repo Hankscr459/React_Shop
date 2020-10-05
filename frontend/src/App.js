475afe53d763d2c72602c30de5c4cf39bbc2ce33
xrudoga <defcw58@gmail.com>
2020-10-05 17:36:49 +0800

    Header & Footer Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import './bootstrap.min.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import  { Container } from 'react-bootstrap'
 
 const App = () => {
   return (
     <>
-      <h1>Welcome To Shop</h1>
+      <Header />
+        <main className='py-3'>
+          <Container>
+            <h1>Welcome To Shop</h1>
+          </Container>
+        </main>
+      <Footer />
     </>
   )
 }
